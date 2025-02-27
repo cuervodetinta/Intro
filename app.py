56cdc3905300cf66fb0c47d328354434d225f85c
cuervodetinta <ghstt.dante@gmail.com>
2025-02-27 10:13:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from PIL import Image
 
 st.title("Hola Mundo!!")
+
 st.header("Wow guys puse texto en una página web")
 st. write("ahora que")
-image = Image.opn('')
+image = Image.opn('IMG_1936.jpg')
+
+st.image(image, caption='miren a mi gata')
