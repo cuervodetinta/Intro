0b1329a4d1b99d69572fb91c5dbfcf08d1985caa
cuervodetinta <ghstt.dante@gmail.com>
2025-02-27 10:21:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ st. write("ahora que")
 image = Image.open('IMG_1936.jpg')
 
 st.image(image, caption='miren a mi gata')
+
+
+texto = st.text_input('bueno pero', 'y se sabe el chiste del bus')
+st.write('pero', texto)
